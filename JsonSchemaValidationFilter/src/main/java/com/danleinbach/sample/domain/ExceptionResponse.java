2d5f2820085d9ade0a81f0e0f7f870429ce16a1a
Daniel Leinbach <leinb1dr@gmail.com>
2013-12-26 11:56:36 -0500

    Updated compiler leve.

--- JsonSchemaValidationFilter/src/main/java/com/danleinbach/sample/domain/ExceptionResponse.java
+++ JsonSchemaValidationFilter/src/main/java/com/danleinbach/sample/domain/ExceptionResponse.java
@@ -9,34 +9,34 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class ExceptionResponse {
 
-	private String subCode;
-	private String errorMessage;
-	private String errorCausedBy;
+  private String subCode;
+  private String errorMessage;
+  private String errorCausedBy;
 
-	@JsonProperty
-	public String getSubCode() {
-		return subCode;
-	}
+  @JsonProperty
+  public String getSubCode() {
+    return subCode;
+  }
 
-	public void setSubCode(String subCode) {
-		this.subCode = subCode;
-	}
+  public void setSubCode(String subCode) {
+    this.subCode = subCode;
+  }
 
-	@JsonProperty
-	public String getErrorMessage() {
-		return errorMessage;
-	}
+  @JsonProperty
+  public String getErrorMessage() {
+    return errorMessage;
+  }
 
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
+  public void setErrorMessage(String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
 
-	@JsonProperty
-	public String getErrorCausedBy() {
-		return errorCausedBy;
-	}
+  @JsonProperty
+  public String getErrorCausedBy() {
+    return errorCausedBy;
+  }
 
-	public void setErrorCausedBy(String errorCausedBy) {
-		this.errorCausedBy = errorCausedBy;
-	}
+  public void setErrorCausedBy(String errorCausedBy) {
+    this.errorCausedBy = errorCausedBy;
+  }
 }
